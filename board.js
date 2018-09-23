f146c67efb9aaf1870006402952cdeed0d38e47e
Diogo Pereira <diogo99p@gmail.com>
2018-09-23 17:41:36 +0100

    added login function

--- board.js
+++ board.js
@@ -1,3 +1,21 @@
-/**
- * 
- */
+"use strict";
+
+var divs = ["loginPageDiv","gameOptionsDiv"]
+
+function userLogin() {
+    showGameOptions();
+}
+
+function showGameOptions() {
+    for(var i=0; i<divs.length; i++)
+        document.getElementById(divs[i]).style.display = "none";
+
+    document.getElementById("gameOptionsDiv").style.display = "block";
+}
+
+function showLoginPage() {
+    for(var i=0; i<divs.length; i++)
+        document.getElementById(divs[i]).style.display = "none";
+
+    document.getElementById("loginPageDiv").style.display = "block";
+}

70810b51ccfde6bc692c0e254e854cb15b59727d
Diogo Pereira <diogo99p@gmail.com>
2018-11-30 12:24:37 +0000

    started configuring server

---
+++ Server/conf.js
@@ -0,0 +1,12 @@
+module.exports.documentRoot = 'Client';
+module.exports.defaultIndex = 'index.html';
+module.exports.port = 8033;
+module.exports.mediaTypes = {
+    'txt':      'text/plain',
+    'html':     'text/html',
+    'css':      'text/css',
+    'js':       'application/javascript',
+    'png':      'image/png',
+    'jpeg':     'image/jpeg',
+    'jpg':      'image/jpeg',
+}
---
+++ Server/index.js
@@ -0,0 +1,5 @@
+const http = require("http");
+const path = require("path");
+const url  = require("url");
+const fs   = require("fs");
+const conf = require("./conf.js"); 
